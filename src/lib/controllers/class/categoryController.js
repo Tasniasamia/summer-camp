95d70cc30c95e2c0f2e94f698e154aee2025fc48
tasniaarhamarham <tasniaarhamarham@gmail.com>
2025-08-31 06:06:48 +0600

    category done

--- src/lib/controllers/class/categoryController.js
+++ src/lib/controllers/class/categoryController.js
@@ -38,6 +38,39 @@ export const postClassCategory = async (req) => {
 
 export const getAllCategory = async (req) => {
   try {
+    const {searchParams} = new URL(req.url);
+    let limit = parseInt(searchParams.get("limit"));
+    let page = parseInt(searchParams.get("page"));
+    if (limit || page) {
+      const [docs, totalDocs] = await Promise.all([
+        prisma.category.findMany({
+          skip: (page - 1) * limit,
+          take: limit,
+        }),
+        prisma.category.count(),
+      ]);
+      const totalPages = Math.ceil(totalDocs / limit);
+      const hasPrevPage = page > 1;
+      const hasNextPage = page < totalPages;
+      return {
+        success: true,
+        msg: "Get data successfully",
+        data: {
+          docs,
+          totalDocs: totalDocs,
+          limit: limit,
+          currentPage: page,
+          hasPrevPage: hasPrevPage,
+          hasNextPage: hasNextPage,
+          totalPages:totalPages,
+          prevPage:page>1?currentPage-1:null,
+          nextPage:page<totalPages?currentPage+1:null
+
+        },
+        status:200
+      };
+    }
+
     const allCategory = await prisma.category.findMany({});
     if (allCategory) {
       return {
@@ -61,7 +94,7 @@ export const putClassCategory = async (req) => {
     }
     if (token) {
       const user = verifyToken(token);
-      const {id,name,image}=await req.json();
+      const { id, name, image } = await req.json();
       if (user?.role === "admin") {
         const updateData = await prisma.category.update({
           where: { id: id },
